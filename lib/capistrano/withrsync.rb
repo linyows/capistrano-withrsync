0221554601324bf94e0e9c6cd1f145e921102459
Ben Tucker <ben@btucker.net>
2015-04-27 15:38:33 -0400

    require shellwords

--- lib/capistrano/withrsync.rb
+++ lib/capistrano/withrsync.rb
@@ -1,5 +1,6 @@
 require 'capistrano/withrsync/version'
 require 'capistrano/withrsync/rake/task'
+require 'shellwords'
 load File.expand_path('../tasks/withrsync.rake', __FILE__)
 
 module Capistrano
