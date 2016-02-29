070dbbf36454d741f89428f70de7f24293b08e07
linyows <linyows@gmail.com>
2016-03-01 07:27:00 +0900

    update test

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,12 +1,15 @@
-describe 'tasks' do
+describe 'task' do
+  let(:user) { 'deploy' }
+  let(:escaped_user) { 'deploy' }
+
   before do
     Rake.application.rake_require 'capistrano/tasks/deploy'
     Rake.application.rake_require 'capistrano/tasks/git'
     Rake.application.rake_require 'capistrano/tasks/withrsync'
 
-    server 'example1.com', roles: %w(web)
-    server 'example2.com', roles: %w(app web)
-    server 'example3.com', roles: %w(db), no_release: true
+    server 'example1.com', user: user, roles: %w(web)
+    server 'example2.com', user: user, roles: %w(app web)
+    server 'example3.com', user: user, roles: %w(db), no_release: true
 
     set :repo_url, 'https://github.com/linyows/capistrano-withrsync.git'
     set :deploy_to, Pathname.new('/var/www/app')
@@ -16,24 +19,67 @@ describe 'tasks' do
     set :release_path, releases_path.join(Time.now.utc.strftime "%Y%m%d%H%M%S")
   end
 
-  describe 'release' do
-    it 'calls rsync commands and git commands' do
+  shared_context :create_src do
+    before do
       allow_any_instance_of(SSHKit::Backend::Local).to receive(:execute).
         with(:git, :clone, nil, fetch(:repo_url), fetch(:rsync_src))
+    end
+  end
+
+  shared_context :check_dir do
+    before do
       allow_any_instance_of(SSHKit::Backend::Local).to receive(:execute).
         with("          if test ! -d tmp/deploy\n            then echo \"Directory does not exist '#{fetch :rsync_src}'\" 1>&2\n            false\n          fi\n", {:verbosity=>0})
+    end
+  end
+
+  shared_context :stage do
+    before do
       allow_any_instance_of(SSHKit::Backend::Local).to receive(:execute).
         with(:git, :fetch, nil, '--quiet --all --prune')
       allow_any_instance_of(SSHKit::Backend::Local).to receive(:execute).
         with(:git, :reset, '--hard origin/')
+    end
+  end
 
+  shared_context :sync do
+    before do
       1.upto 2 do |i|
         host = "example#{i}.com"
         allow_any_instance_of(SSHKit::Backend::Local).to receive(:execute).
           with(:rsync, '--recursive --delete --delete-excluded --exclude .git* --exclude .svn*',
-            'tmp/deploy/', "#{host}:#{fetch(:deploy_to)}/shared/deploy")
+            'tmp/deploy/', "#{escaped_user}@#{host}:#{fetch(:deploy_to)}/shared/deploy")
       end
+    end
+  end
 
+  describe 'sync' do
+    include_context :create_src
+    include_context :check_dir
+    include_context :stage
+    include_context :sync
+
+    it 'synchronizes to remote from local' do
+      quietly { invoke :'rsync:sync' }
+    end
+
+    context 'includes space in username' do
+      let(:user) { 'deploy user' }
+      let(:escaped_user) { "deploy\ user" }
+
+      it 'escapes username' do
+        quietly { invoke :'rsync:sync' }
+      end
+    end
+  end
+
+  describe 'release' do
+    include_context :create_src
+    include_context :check_dir
+    include_context :stage
+    include_context :sync
+
+    it 'releases from temp directory on remote' do
       allow_any_instance_of(SSHKit::Backend::Netssh).to receive(:execute).
         with(:rsync, '--archive --acls --xattrs',
           "#{fetch(:shared_path).join('deploy')}/", "#{fetch :release_path}/")
